6525404d463d1c1eb62545ee83f79d923a91eda5
Vladyslav Sorokin <Vladyslav.Sorokin@infineon.com>
2025-06-05 18:03:53 +0200

    advanced tests

--- giga-r1-serial-transfer-tests.ino
+++ giga-r1-serial-transfer-tests.ino
@@ -1,10 +1,14 @@
-#define ROUND_NUM 5
+#define ROUND_NUM 10
+#define READY_CODE 127
+#define MIN_CHUNK_TEST 8
+#define MAX_CHUNK_TEST 256
 
-const uint16_t buf_size = 20000;
+static uint8_t reset_flag = 0;
+const uint16_t buf_size = 20480;
 uint16_t buffer[buf_size];
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(2000000);
     while (!Serial) {
         delay(1);
     }
@@ -16,6 +20,13 @@ void setup() {
 }
 
 void loop() {
+    // prepare matlab sync
+    wait_trigger();
+    if (reset_flag == 1) {
+        Serial.write(READY_CODE);
+        reset_flag = 0;
+    }
+
     // trial #1
     wait_trigger();
     for (uint16_t i = 0; i < ROUND_NUM; i++) {
@@ -23,6 +34,12 @@ void loop() {
     }
     
     // trial #2
+    for (uint16_t chunk = 8; chunk <= MAX_CHUNK_TEST; chunk *= 2) {
+        wait_trigger();
+        for (uint16_t i = 0; i < ROUND_NUM; i++) {
+            transfer_serial_data_advanced(buffer, buf_size, chunk);
+        }
+    }
 }
 
 void wait_trigger() {
@@ -31,6 +48,10 @@ void wait_trigger() {
         while (Serial.available() == 0);
         serial_buf = Serial.read();
         if (serial_buf == 's') {
+            return;
+        }
+        if (serial_buf == 'r') {
+            reset_flag = 1;
             return;
         }
     }
@@ -42,6 +63,9 @@ void transfer_serial_data_basic(const uint16_t* data, const uint16_t size) {
     }
 }
 
-void transfer_serial_data_advanced(const uint16_t* data, const uint16_t size, const uint16_t chunk_size) {
-    return;
+void transfer_serial_data_advanced(const uint16_t* data, const uint16_t data_length, const uint16_t chunk_size_byte) {
+    for (uint16_t i = 0; i < (data_length*2); i += chunk_size_byte) {
+        uint16_t transfer_size = ((data_length*2) - i < chunk_size_byte) ? (data_length*2 - i) : chunk_size_byte;
+        Serial.write((const uint8_t *) data + i, transfer_size);
+    }
 }
--- transfer_tests.m
+++ transfer_tests.m
@@ -5,30 +5,66 @@ clc;
 
 %% Settings
 ARDUINO_PORT = 'COM8';
-ARDUINO_BAUDRATE = 115200;
+ARDUINO_BAUDRATE = 2000000;
 
-BUF_SIZE = 20000;
-ROUND_NUM = 5;
+BUF_SIZE = 20480;
+ROUND_NUM = 10;
+READY_CODE = 127;
+MAX_CHUNK_TEST = 256;
+MIN_CHUNK_TEST = 8;
 
 %% Setup
 arduino = serialport(ARDUINO_PORT, ARDUINO_BAUDRATE);
+arduino.Timeout = 10;
 buf = zeros(2, BUF_SIZE);
+write(arduino, 'r', "char"); % start
+code = read(arduino, 1, 'uint8');
+if code ~= READY_CODE
+    error("Arduino Synchronization Failed! " + ...
+        "Expected 'r' symbol from Arduino. " + ...
+        "It happens if you shut down MATLAB code manually, " + ...
+        "restart MATLAB script couple of times to fix it.");
+end
 
 %% Trials
+trial_num = 1 + (log2(MAX_CHUNK_TEST)-log2(MIN_CHUNK_TEST)+1);
+trial_time = zeros(1, trial_num);
+trial_speed_kbps = zeros(1, trial_num);
+
+% Trial #1
 write(arduino, 's', "char"); % start
 tic;
 for j = 1:ROUND_NUM
     for i = 1:BUF_SIZE
-        buf(:,i) = read(arduino, 2, 'uint8');
+        read(arduino, 2, 'uint8');
     end
 end
-trial1_time = toc;
-fprintf("Trial #1 (Basic sample by sample): " + round(calculate_time(trial1_time, BUF_SIZE, ROUND_NUM)) + "kbps");
+trial_time(1) = toc;
+trial_speed_kbps(1) = round(calculate_speed_kbps (trial_time(1), BUF_SIZE, ROUND_NUM));
+fprintf("Trial #1 (Basic sample by sample): " + trial_speed_kbps(1) + "kbps\n");
+
+% Trial #2
+for trial = 2:trial_num
+    chunk = MIN_CHUNK_TEST* 2^(trial - 2);
+    chunk_num = (BUF_SIZE*2)/chunk;
+
+    write(arduino, 's', "char"); % start
+    tic;
+    for j = 1:ROUND_NUM
+        for i = 1:chunk_num
+            read(arduino, chunk, 'uint8');
+        end
+    end
+
+    trial_time(trial) = toc;
+    trial_speed_kbps(trial) = round(calculate_speed_kbps (trial_time(trial), BUF_SIZE, ROUND_NUM));
+    fprintf("Trial #" + trial + " (By " + chunk + "byte chunks): " + trial_speed_kbps(trial) + "kbps\n");
+end
 
 arduino = [];
 
 %% Functions
-function time_kbps = calculate_time(time_sec, buf_size, round_num)
+function time_kbps = calculate_speed_kbps(time_sec, buf_size, round_num)
     total_bits = buf_size * round_num * 16;
     time_kbps = (total_bits / time_sec) / 1000;
 end
